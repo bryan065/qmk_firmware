3833944145d4b1907567107be0a74098f28d0c9e
bryan065 <30362590+bryan065@users.noreply.github.com>
2020-04-25 02:21:44 -0400

    Updated keymaps and programming

--- keyboards/dz60/keymaps/unicorn-via/config.h
+++ keyboards/dz60/keymaps/unicorn-via/config.h
@@ -1,57 +1,47 @@
 #undef MANUFACTURER
 #undef PRODUCT
-#undef DESCRIPTION
 #undef DEBOUNCE
+#undef PRODUCT_ID
 
-#define MANUFACTURER    Anahein Electronics
-#define PRODUCT         Unicorn RX-0 SplitSpace Keyboard
-
-//disable VIA backlight function
-#define RGB_BACKLIGHT_ENABLED 0
+#define MANUFACTURER    Anaheim Electronics
+#define PRODUCT         DZ60 Unicorn Keyboard
+#define PRODUCT_ID      0x2260
 
 #define NO_ACTION_MACRO
 #define NO_ACTION_FUNCTION
-
-/* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
 #define DEBOUNCE 3
-
-// disable backlight after timeout in minutes, 0 = no timeout
-#define RGB_BACKLIGHT_DISABLE_AFTER_TIMEOUT 15
+#define TIMEOUT_DELAY 900
 
 //RGB Matrix Effects
-#define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_CYCLE_LEFT_RIGHT
-#define RGB_MATRIX_KEYPRESSES               // Keypress effects
-#define RGB_MATRIX_FRAMEBUFFER_EFFECTS      // Frame buffer effects
-  //disable standard effects
-  #define DISABLE_RGB_MATRIX_ALPHAS_MODS
-  #define DISABLE_RGB_MATRIX_BAND_SAT
-  #define DISABLE_RGB_MATRIX_BAND_VAL
-  #define DISABLE_RGB_MATRIX_BAND_PINWHEEL_SAT
-  #define DISABLE_RGB_MATRIX_BAND_PINWHEEL_VAL
-  #define DISABLE_RGB_MATRIX_BAND_SPIRAL_SAT
-  #define DISABLE_RGB_MATRIX_BAND_SPIRAL_VAL
-  #define DISABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
-  #define DISABLE_RGB_MATRIX_RAINDROPS
-  //disable keypress effects
-  #define DISABLE_RGB_MATRIX_SOLID_REACTIVE
-  #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
-  #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
-  #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
-  #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
-  #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS
-  #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
-  #define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXU
-  #define DISABLE_RGB_MATRIX_MULTISPLASH
+#define RGB_MATRIX_KEYPRESSES                                // Keypress effects
+#define RGB_MATRIX_FRAMEBUFFER_EFFECTS                       // Frame buffer effects
+#define RGB_MATRIX_STARTUP_MODE RGB_MATRIX_CYCLE_LEFT_RIGHT  //default mode
+//disable standard effects
+#define DISABLE_RGB_MATRIX_ALPHAS_MODS
+#define DISABLE_RGB_MATRIX_BAND_SAT
+#define DISABLE_RGB_MATRIX_BAND_VAL
+#define DISABLE_RGB_MATRIX_BAND_PINWHEEL_SAT
+#define DISABLE_RGB_MATRIX_BAND_PINWHEEL_VAL
+#define DISABLE_RGB_MATRIX_BAND_SPIRAL_SAT
+#define DISABLE_RGB_MATRIX_BAND_SPIRAL_VAL
+#define DISABLE_RGB_MATRIX_JELLYBEAN_RAINDROPS
+#define DISABLE_RGB_MATRIX_RAINDROPS
+//disable keypress effects
+#define DISABLE_RGB_MATRIX_SOLID_REACTIVE
+#define DISABLE_RGB_MATRIX_SOLID_REACTIVE_SIMPLE
+#define DISABLE_RGB_MATRIX_SOLID_REACTIVE_WIDE
+#define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTIWIDE
+#define DISABLE_RGB_MATRIX_SOLID_REACTIVE_CROSS
+#define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTICROSS
+#define DISABLE_RGB_MATRIX_SOLID_REACTIVE_NEXUS
+#define DISABLE_RGB_MATRIX_SOLID_REACTIVE_MULTINEXU
+#define DISABLE_RGB_MATRIX_MULTISPLASH
 
 //RGB Matrix Config
-#define RGB_DISABLE_AFTER_TIMEOUT 0         // number of ticks to wait until disabling effects
 #define RGB_DISABLE_WHEN_USB_SUSPENDED true // turn off effects when suspended
-#define RGB_MATRIX_LED_PROCESS_LIMIT 4
-#define RGB_MATRIX_LED_FLUSH_LIMIT 26       // limit refresh rate to ~45fps
 #define DRIVER_LED_TOTAL 100                // The number of LEDs connected
 #define RGB_DI_PIN E2                       // Assign to pin E2 on DZ60 board
 #define RGBLIGHT_SLEEP
 #define RGB_MATRIX_MAXIMUM_BRIGHTNESS 255
-#define RGB_MATRIX_STARTUP_HUE 0
-#define RGB_MATRIX_STARTUP_SAT 255
-#define RGB_MATRIX_STARTUP_VAL 240
+#define RGB_MATRIX_STARTUP_VAL 255
+#define RGB_MATRIX_STARTUP_SPD 64
--- keyboards/dz60/keymaps/unicorn-via/keymap.c
+++ keyboards/dz60/keymaps/unicorn-via/keymap.c
@@ -1,15 +1,52 @@
 #include QMK_KEYBOARD_H
 
+static uint16_t   dynamic_loop_timer;
+static uint16_t   idle_timer;
+static bool       timeout;
+static uint16_t   timeout_delay = TIMEOUT_DELAY;
+static uint16_t   fadetime;
+static uint16_t   rgb_matrix_v;
+
 enum keyboard_layers {
   _BASE = 0, 	// Base Layer
   _FUNC,     	// Function Layer
   _RGB,     	// RGB control Layer
-  _SYS			  // System control layer (reset, sleep, etc)
-};
+_SYS	};		  // System control layer (reset, sleep, etc)
+
 
 enum my_keycodes {
-  KC_00 = SAFE_RANGE
-};
+  KC_00 = SAFE_RANGE };
+
+
+//LED TO MATRIX MAP
+led_config_t g_led_config = {
+    {
+        // Key Matrix to LED Index
+        {  0,      1,      2,      3,      4,      5,      6,      7,  8,      9, 10, 11,     12, NO_LED,     13 },
+        { 14, NO_LED,     15,     16,     17,     18,     19,     20, 21,     22, 23, 24,     25,     26,     27 },
+        { 28, NO_LED,     29,     30,     31,     32,     33,     34, 35,     36, 37, 38,     39,     40, NO_LED },
+        { 41, NO_LED,     42,     43,     44,     45,     46,     47, 48,     49, 50, 51, NO_LED,     52,     53 },
+        { 54,     55, NO_LED,     56,     57, NO_LED,     58, NO_LED, 59, NO_LED, 60, 61,     62,     63,     64 }
+    }, {
+        // LED Index to Physical Position
+        { 216,   0 }, { 192,   0 }, { 176,   0 }, { 160,   0 }, { 144,   0 }, { 128,   0 }, { 112,   0 }, {  96,   0 }, {  80,   0 }, {  64,   0 }, {  48,   0 }, {  32,   0 }, {  16,   0 },               {   0,   0 }, 
+        { 220,  16 },               { 200,  16 }, { 184,  16 }, { 168,  16 }, { 152,  16 }, { 136,  16 }, { 120,  16 }, { 104,  16 }, {  88,  16 }, {  72,  16 }, {  56,  16 }, {  40,  16 }, {  24,  16 }, {   0,  16 },
+        { 214,  32 },               { 188,  32 }, { 172,  32 }, { 156,  32 }, { 140,  32 }, { 124,  32 }, { 108,  32 }, {  92,  32 }, {  76,  32 }, {  60,  32 }, {  44,  32 }, {  28,  32 }, {   6,  32 },
+        { 224,  48 },               { 208,  48 }, { 186,  48 }, { 164,  48 }, { 148,  48 }, { 132,  48 }, { 116,  48 }, { 100,  48 }, {  84,  48 }, {  68,  48 }, {  52,  48 },               {  24,  48 }, {   0,  48 },
+        { 224,  64 }, { 208,  64 }, { 192,  64 },               { 128,  64 }, { 116,  64 }, { 100,  64 },                                           {  58,  64 }, {  50,  64 }, {  42,  64 }, {  24,  64 }, {   0,  64 },
+        //underglow
+        { 242,  64 }, { 216,  64 }, { 192,  64 }, { 176,  64 }, { 160,  64 }, { 144,  64 }, { 128,  64 }, { 112,  64 }, {  96,  64 }, {  80,  64 }, {  64,  64 }, {  48,  64 }, {  32,  64 }, {  16,  64 }, {   0,  64 },
+        {   0,  16 }, {  16,  16 }, {  32,  16 },                                    {  88,   0 }, { 104,   0 }, { 120,   0 }, { 136,   0 },                                    { 176,  16 }, { 192,  16 }, { 216,  16 }
+    }, {
+        // LED Index to Flag
+        1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1,
+        1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1,
+        1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,    1,
+        1,    4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1, 1,
+        1, 1, 1,     4,   1,    4, 1, 1, 1, 1, 1,
+        // Underglow LED's
+        2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2
+    } };
 
 
   /* RGBLIGHT strip wiring. LED #75 to #99 is for underglow
@@ -32,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   *  Backspace & left shift is full size compared to default layout
   *  2.75u * 2.25u space bars are swapped (longer one on left hand side)
   * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐
-  * │esc│ 1 │ 2 │ 3 │ 4 │ 5 │ 6 │ 7 │ 8 │ 9 │ 10│ 11│ 12│ bkspc │
+  * │Gsc│ 1 │ 2 │ 3 │ 4 │ 5 │ 6 │ 7 │ 8 │ 9 │ 10│ 11│ 12│ bkspc │
   * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤
   * │tab  │ q │ w │ e │ r │ t │ y │ u │ i │ o │ p │ [ │ ] │ \   │
   * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┤
@@ -54,7 +91,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐
   * │~  │F1 │F2 │F3 │F4 │F5 │F6 │F7 │F8 │F9 │F10│F11│F12│delete │
   * ├───┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─────┤
-  * │     │   │   │   │   │   │   │ 7 │ 8 │ 9 │   │   │   │     │
+  * │`    │   │   │   │   │   │   │ 7 │ 8 │ 9 │   │   │   │     │
   * ├─────┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴┬──┴─────┤
   * │      │   │   │   │   │   │   │ 4 │ 5 │ 6 │   │   │ insert │
   * ├──────┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴─┬─┴────┬───┤
@@ -64,11 +101,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   * └────┴────┴────┴──────────┴────┴────────┴───┴───┴───┴───┴───┘
   */
   [_FUNC] = LAYOUT(
-    KC_TILD, KC_F1, KC_F2, KC_F3, KC_F4, KC_F5, KC_F6, KC_F7, KC_F8, KC_F9, KC_F10, KC_F11, KC_F12, _______, KC_DEL,
-    _______, _______, _______, _______, _______, _______, _______, KC_P7, KC_P8, KC_P9, _______, _______, _______, _______,
-    _______, _______, _______, _______, _______, _______, _______, KC_P4, KC_P5, KC_P6, _______, _______, KC_INS,
-    _______, _______, _______, _______, _______, _______, _______, _______, KC_P1, KC_P2, KC_P3, _______, KC_NLCK, KC_PGUP,
-    _______, _______, _______, _______, _______, KC_P0, _______, _______, KC_HOME, KC_PGDN, KC_END
+    KC_TILD , KC_F1  , KC_F2  , KC_F3  , KC_F4  , KC_F5  , KC_F6  , KC_F7, KC_F8, KC_F9, KC_F10 , KC_F11 , KC_F12 , _______, KC_DEL,
+    KC_GRV, _______, _______, _______, _______, _______, _______, KC_P7, KC_P8, KC_P9, _______, _______, _______, _______,
+    _______ , _______, _______, _______, _______, _______, _______, KC_P4, KC_P5, KC_P6, _______, _______, KC_INS,
+    _______ , _______, _______, _______, _______, _______, _______, _______, KC_P1, KC_P2, KC_P3, _______, KC_NLCK, KC_PGUP,
+    _______ , _______, _______, _______, _______, KC_P0, _______, _______, KC_HOME, KC_PGDN, KC_END
     ),
   /* RGB layer (RMD = RGB_RMOD, MSW = RGB_M_SW)
   * ┌───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───┬───────┐
@@ -109,44 +146,76 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______
-    ),
-};
+    ), };
 
-//LED TO MATRIX MAP
-led_config_t g_led_config = {
-    {
-        // Key Matrix to LED Index
-        {  0,      1,      2,      3,      4,      5,      6,      7,  8,      9, 10, 11,     12, NO_LED,     13 },
-        { 14, NO_LED,     15,     16,     17,     18,     19,     20, 21,     22, 23, 24,     25,     26,     27 },
-        { 28, NO_LED,     29,     30,     31,     32,     33,     34, 35,     36, 37, 38,     39,     40, NO_LED },
-        { 41, NO_LED,     42,     43,     44,     45,     46,     47, 48,     49, 50, 51, NO_LED,     52,     53 },
-        { 54,     55, NO_LED,     56,     57, NO_LED,     58, NO_LED, 59, NO_LED, 60, 61,     62,     63,     64 }
-    }, {
-        // LED Index to Physical Position
-        { 216,   0 }, { 192,   0 }, { 176,   0 }, { 160,   0 }, { 144,   0 }, { 128,   0 }, { 112,   0 }, {  96,   0 }, {  80,   0 }, {  64,   0 }, {  48,   0 }, {  32,   0 }, {  16,   0 }, {   0,   0 },
-        { 220,  16 }, { 200,  16 }, { 184,  16 }, { 168,  16 }, { 152,  16 }, { 136,  16 }, { 120,  16 }, { 104,  16 }, {  88,  16 }, {  72,  16 }, {  56,  16 }, {  40,  16 }, {  24,  16 }, {   4,  16 },
-        { 214,  32 }, { 188,  32 }, { 172,  32 }, { 156,  32 }, { 140,  32 }, { 124,  32 }, { 108,  32 }, {  92,  32 }, {  76,  32 }, {  60,  32 }, {  44,  32 }, {  28,  32 },               {   6,  32 },
-        { 224,  48 },               { 208,  48 }, { 186,  48 }, { 164,  48 }, { 148,  48 }, { 132,  48 }, { 116,  48 }, { 100,  48 }, {  84,  48 }, {  68,  48 }, {  52,  48 }, {  36,  48 }, {   9,  48 },
-        { 224,  64 }, { 208,  64 }, { 192,  64 },                                           { 176,  64 },                             { 160,  64 }, { 102,  64 }, {  42,  64 }, {  22,  64 }, {   2,  64 }
-    }, {
-        // LED Index to Flag
-        1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1,
-        1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1,
-        1, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 4,    1,
-        1,    4, 4, 4, 4, 4, 4, 4, 4, 4, 4, 1, 1,
-        1, 1, 1,     4, 1,      4, 1, 1, 1, 1, 1,
-        // Underglow LED's
-        2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2
-    }
-};
-
-//disable RGB when USB is suspended
+//===============USB Suspend Functions====================//
 void suspend_power_down_kb(void) {
-    rgb_matrix_set_suspend_state(true);
-    suspend_power_down_user();
+  rgb_matrix_set_suspend_state(true);
+  suspend_power_down_user(); }
+void suspend_wakeup_init_kb(void) {
+  rgb_matrix_set_suspend_state(false);
+  suspend_wakeup_init_user(); }
+//===============USB Suspend Functions====================//
+
+
+//===============Custom Functions=========================//
+//fade in animation
+void rgb_matrix_fade_in(void) {
+  //animation
+  uint16_t rgb_matrix_v = rgb_matrix_config.hsv.v;
+  for (uint16_t i = 0; i < rgb_matrix_v; i++) {     //fade from 0 to the current V value
+    rgb_matrix_sethsv_noeeprom( rgb_matrix_config.hsv.h, rgb_matrix_config.hsv.s, i );
+    matrix_scan();
+  } }
+
+//fade out animation
+void rgb_matrix_fade_out(void) {
+  //animation
+  rgb_matrix_v = rgb_matrix_config.hsv.v;  //grab current V value to restore
+  for (uint16_t i = rgb_matrix_v; i > 0; i--) {     //fade from the current V value to 0
+    rgb_matrix_sethsv_noeeprom( rgb_matrix_config.hsv.h, rgb_matrix_config.hsv.s, i );
+    matrix_scan();
+  }
+  rgb_matrix_disable_noeeprom();
+  rgb_matrix_sethsv_noeeprom( rgb_matrix_config.hsv.h, rgb_matrix_config.hsv.s, rgb_matrix_v ); }  //restore V value
+//===============Custom Functions=========================//
+
+
+//===============Initialization Functions=================//
+void keyboard_post_init_user(void) {
+  dynamic_loop_timer = timer_read();
+  rgb_matrix_fade_in(); }
+//===============Initialization Functions=================//
+
+
+//===============Main Loops===============================//
+//RGB Matrix scan
+void rgb_matrix_indicators_user (void) {
+  //RGB timout, running every second
+  if (timer_elapsed(dynamic_loop_timer) > 1000) {
+    idle_timer++;
+    dynamic_loop_timer = timer_read();
+    if (idle_timer >= timeout_delay) {
+      rgb_matrix_fade_out();
+      idle_timer = 0;
+      timeout = true;
+    }
+  } //End of RGB timeout
+  
+  //RGB Matrix with fade-in animation once flagged by "timeout"
+  if ((timeout == false) && fadetime < rgb_matrix_v) {
+    fadetime++;
+    rgb_matrix_sethsv_noeeprom( rgb_matrix_config.hsv.h, rgb_matrix_config.hsv.s, fadetime );
+  }
 }
 
-void suspend_wakeup_init_kb(void) {
-    rgb_matrix_set_suspend_state(false);
-    suspend_wakeup_init_user();
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  idle_timer = 0;          //reset idle timer
+  if (timeout == true) {   //flag key pressed
+    timeout = false;
+    fadetime = 0;
+    rgb_matrix_enable_noeeprom();
+  }
+  return true;
 }
+//===============Main Loops===============================//
