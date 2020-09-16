5742d4d526638d9c12b4f9a2493ca52818c05722
bryan065 <30362590+bryan065@users.noreply.github.com>
2020-09-15 21:02:34 -0400

    Update keymap.c

--- keyboards/kbdfans/kbd67/mkii_soldered/keymaps/cyber77/keymap.c
+++ keyboards/kbdfans/kbd67/mkii_soldered/keymaps/cyber77/keymap.c
@@ -138,16 +138,16 @@ void ql_reset(qk_tap_dance_state_t *state, void *user_data);
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_BASE] = LAYOUT_all( /* Base */
 	KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_DEL,  TD(TD_MUTE),
-	KC_TAB,            KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,          KC_BSLS, LALT(KC_PSCR),
+	KC_TAB,            KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,          KC_BSLS, KC_DEL,
 	TD(TD_SYS_QL),             KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,           KC_INS,
-	KC_LSFT,           KC_BSLS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   KC_DEL,
+	KC_LSFT,           KC_BSLS, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,   LALT(KC_PSCR),
 	KC_LCTL,           KC_LGUI, KC_LALT,          KC_SPC,           MO(_FUNC),        KC_SPC,           MO(_RGB), MO(_FUNC),        KC_LEFT, KC_DOWN, KC_RGHT
   ),
   [_FUNC] = LAYOUT_all( /* FN */
 	KC_TILD,   KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,_______, _______, _______,
-	KC_GRV , _______, KC_UP, _______, _______, _______, _______, KC_P7, KC_P8, KC_P9, _______, _______, _______,          LGUI(KC_PAUS), LGUI(KC_PSCR),
-	_______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, KC_P4, KC_P5, KC_P6, _______, _______,          KC_PENT,          DM_PLY1,
-	_______, _______, _______, _______, _______, _______, _______, KC_P1, KC_P2, KC_P3, _______, _______, KC_NLCK,          KC_PGUP, DM_PLY2,
+	KC_GRV , _______, KC_UP, _______, _______, _______, _______, KC_P7, KC_P8, KC_P9, _______, _______, _______,          LGUI(KC_PAUS), DM_PLY1,
+	_______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, KC_P4, KC_P5, KC_P6, _______, _______,          KC_PENT,          DM_PLY2,
+	_______, _______, _______, _______, _______, _______, _______, KC_P1, KC_P2, KC_P3, _______, _______, KC_NLCK,          KC_PGUP, LGUI(KC_PSCR),
 	_______, _______, _______,          _______,          _______,          KC_P0,          KC_PDOT, _______,          KC_HOME, KC_PGDN, KC_END
   ),
   [_RGB] = LAYOUT_all(
@@ -159,9 +159,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   ),
   [_SYS] = LAYOUT_all(
     _______, AU_SONG1, AU_SONG2,   AU_SONG3, _______, _______,   _______, _______, _______,   _______, _______, _______,  _______, _______, _______, KC_SLEP,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          RESET, DM_RSTP,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          KC_MAKE,          DM_REC1,
-    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, AU_TOG,          _______, DM_REC2,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          RESET, DM_REC1,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,          KC_MAKE,          DM_REC2,
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, AU_TOG,          _______, DM_RSTP,
     _______, KC_WINLOCK, _______,          _______,          _______,          _______,          _______, _______,          _______, _______, _______
   )
 };
