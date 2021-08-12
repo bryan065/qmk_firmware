05705853cf08fece5fb172ecf20f0f35a8ef49eb
bryan065 <30362590+bryan065@users.noreply.github.com>
2021-08-12 09:38:01 -0400

    Initial GHero20 Support
    update

---
+++ keyboards/handwired/GHero20/keymaps/default/keymap.c
@@ -0,0 +1,48 @@
+/* Copyright 2021 bryan065
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+#include QMK_KEYBOARD_H
+
+// Defines names for use in layer keycodes and the keymap
+enum layer_names {
+    _BASE,
+    _FN
+};
+
+void matrix_init_user(void) {
+}
+
+void matrix_scan_user(void) {
+}
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+    /* Base */
+    [_BASE] = LAYOUT(
+		KC_1, KC_2, KC_3, KC_4, KC_5, 
+		KC_Q, KC_W, KC_E, KC_R, KC_T, 
+		KC_DOWN, KC_UP, KC_D, 
+		KC_C, KC_A, KC_H, KC_V
+    ),
+    [_FN] = LAYOUT(
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, 
+		KC_TRNS, KC_TRNS, KC_TRNS, 
+		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
+    )
+};
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+	return true;
+}
