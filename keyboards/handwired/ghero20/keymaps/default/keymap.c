8ffbdac56ab48cf31d9ed05bfe509578fc778627
bryan065 <30362590+bryan065@users.noreply.github.com>
2021-09-03 23:11:04 -0400

    Cleanup code to prep for PR
    Clean up code for PR

--- keyboards/handwired/ghero20/keymaps/default/keymap.c
+++ keyboards/handwired/ghero20/keymaps/default/keymap.c
@@ -36,17 +36,3 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     )
 };
-
-void matrix_init_user(void) {
-}
-
-void matrix_scan_user(void) {
-}
-
-void keyboard_post_init_user(void) {
-  // Customise these values to desired behaviour
-  //debug_enable=true;
-  //debug_matrix=true;
-  //debug_keyboard=true;
-  //debug_mouse=true;
-}
